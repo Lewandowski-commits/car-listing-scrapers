58d7f36dfa4e9f6698600f8955049f600b76ec76
Lewandowski-commits <64804900+Lewandowski-commits@users.noreply.github.com>
2021-01-24 12:30:46 +0100

    add scrape_olx function

--- Polish.py
+++ Polish.py
@@ -95,7 +95,7 @@ def scrape_otomoto(search_url: str = "https://www.otomoto.pl/osobowe/hyundai/cou
             d['city'].append(
                 car.find('span', class_='ds-location-city').text)
 
-        print('Parsed page {} of {}'.format(str(page), str(lastPage)))
+        print('Parsed page {} of {} otomoto.pl'.format(str(page), str(lastPage)))
 
     # create the dataframe object and return it
     df_otomoto = DataFrame({key: pd.Series(value) for key, value in d.items()})
@@ -105,10 +105,86 @@ def scrape_otomoto(search_url: str = "https://www.otomoto.pl/osobowe/hyundai/cou
     return df_otomoto
 
 
+def scrape_olx(search_url: str = "https://www.olx.pl/motoryzacja/samochody/hyundai/coupe/?search%5Bfilter_float_year%3Afrom%5D=2006&view=list"):
+    # access the link
+    res = requests.get(search_url)
+    res.raise_for_status()
+
+    # create the BS object
+    carSoup = BeautifulSoup(res.text, features='lxml')
+
+    # check how many pages are there to later loop over
+    try:
+        lastPage = int(carSoup.findAll("a", class_="block br3 brc8 large tdnone lheight24")[-1].text)
+    except:
+        lastPage = 1
+
+    print(lastPage)
+
+    # create the dict upon which the dataframe will be built
+    d = {'name': [],
+         'year': [],
+         'price': [],
+         'currency': [],
+         'mileage (km)': [],
+         'fuel type': [],
+         'disp (cm3)': [],
+         'city': [],
+         'link': []}
+
+    # Loop over all the pages, load them, get the list of all car
+    for page in range(1, lastPage + 1):
+
+        # check if the links need to be modified when looping over
+        if lastPage > 2:
+            res = requests.get(search_url + '&page=' + str(page))
+        else:
+            res = requests.get(search_url)
+
+        # access the link
+        res.raise_for_status()
+
+        # create the BS object and select all listings
+        currentPage = BeautifulSoup(res.text, features='lxml')
+        carList = currentPage.find_all("div", class_="offer-wrapper")
+
+        # loop over all  listings on the page
+        for car in carList:
+
+            # scrape as much information from the listings as possible and put them in the dictionary
+
+            d['name'].append(
+                car.find_all('strong')[0].text)
+
+            d['link'].append(
+                car.find('a')['href'])
+
+            d['price'].append(
+                float(car.find_all('strong')[1].text.replace("zł", "").replace(" ", "").strip()))
+
+            d['currency'].append("PLN")
+
+            d['city'].append(
+                car.find('i', {"data-icon": "location-filled"}).text)
+
+        print('Parsed page {} of {} olx.pl'.format(str(page), str(lastPage)))
+
+    # create the dataframe object and return it
+    df_olx = DataFrame({key: pd.Series(value) for key, value in d.items()})
+
+    df_olx.drop_duplicates(inplace=True)
+
+    return df_olx
+
+
 if __name__ == "__main__":
     currtime = datetime.now().strftime('D%d-%m-%Y T%H-%M-%S')
+
     try:
-        scrape_otomoto(input("Please provide a search link: ")).to_csv(f"data/{currtime}.csv", index=False)
+        joint_results = scrape_otomoto(input("Please provide otomoto.pl search results link: ")).append(
+            scrape_olx(input("Please provide olx.pl search results link: "))
+        )
+        joint_results.to_csv(f"data/{currtime}.csv", index=False)
         print("Success!")
     except:
         print("Failed!")
