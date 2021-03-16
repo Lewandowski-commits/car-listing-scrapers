107d46aff0bdc4f8db3abab8ba6984dfcc761df2
lewandowskicommits <vesilth@gmail.com>
2021-03-16 22:49:49 +0100

    rewrote function to parse makes and models

--- utilities.py
+++ utilities.py
@@ -15,45 +15,55 @@ def parse_engine_capacities_from_names(data: pd.DataFrame,
     data[name_col] = data[name_col].str.strip()
     return data
 
+
+car_makes = ("Volkswagen",
+             "Opel",
+             "BMW",
+             "Ford",
+             "Audi",
+             "Mercedes - Benz",
+             "Mercedes Benz",
+             "Mercedes",
+             "Toyota",
+             "Renault",
+             "Škoda",
+             "Skoda",
+             "Peugeot",
+             "Citroën",
+             "Citroen",
+             "Hyundai",
+             "Kia",
+             "Volvo",
+             "Nissan",
+             "Seat",
+             "Fiat",
+             "Mazda",
+             "Honda",
+             "Suzuki",
+             "Mitsubishi",
+             "Dacia",
+             "Jeep",
+             "Chevrolet",
+             "Mini",
+             "Alfa Romeo",
+             "Porsche")
+
+
 def parse_makes_and_models_from_names(data: pd.DataFrame,
                                       name_col: str = "name",
                                       make_col: str = "make",
                                       model_col: str = "model"):
-    data[make_col] = data[name_col].apply(lambda x: x)
+    def _parse_makes(input_string):
+        result_matches = [x for x in input_string.split() if x in car_makes]
+        print(result_matches)
+
+        return result_matches
+
+    data[make_col] = data[name_col].apply(lambda x: _parse_makes(x))
     return data
 
-def car_makes():
-    return ["Volkswagen",
-           "Opel",
-           "BMW",
-           "Ford",
-           "Audi",
-           "Mercedes - Benz",
-           "Toyota",
-           "Renault",
-           "Škoda",
-           "Škoda",
-           "Peugeot",
-           "Citroën",
-           "Citroën",
-           "Hyundai",
-           "Kia",
-           "Volvo",
-           "Nissan",
-           "Seat",
-           "Fiat",
-           "Mazda",
-           "Honda",
-           "Suzuki",
-           "Mitsubishi",
-           "Dacia",
-           "Jeep",
-           "Chevrolet",
-           "Mini",
-           "Alfa Romeo",
-           "Porsche"]
 
 if __name__ == "__main__":
     data = scrapers.scrape_otomoto()
-    print(parse_engine_capacities_from_names(data))
+    # print(parse_engine_capacities_from_names(data))
     print(parse_makes_and_models_from_names(data))
