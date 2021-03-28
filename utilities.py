324ef7ccf0dd8a0bb1c92b074ef3a8575b092443
lewandowskicommits <vesilth@gmail.com>
2021-03-28 12:14:05 +0200

    replaced hardcoded list of makes with API call

--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import re
 import scrapers
+import json
+import requests
 
 
 # function that when provided a dataframe, parses engine capacities from the dataframe series that contain their names
@@ -15,44 +17,20 @@ def parse_engine_capacities_from_names(data: pd.DataFrame,
     data[name_col] = data[name_col].str.strip()
     return data
 
-
-car_makes = ("Volkswagen",
-             "Opel",
-             "BMW",
-             "Ford",
-             "Audi",
-             "Mercedes - Benz",
-             "Mercedes Benz",
-             "Mercedes",
-             "Toyota",
-             "Renault",
-             "Škoda",
-             "Skoda",
-             "Peugeot",
-             "Citroën",
-             "Citroen",
-             "Hyundai",
-             "Kia",
-             "Volvo",
-             "Nissan",
-             "Seat",
-             "Fiat",
-             "Mazda",
-             "Honda",
-             "Suzuki",
-             "Mitsubishi",
-             "Dacia",
-             "Jeep",
-             "Chevrolet",
-             "Mini",
-             "Alfa Romeo",
-             "Porsche")
-
-
 def parse_makes_and_models_from_names(data: pd.DataFrame,
                                       name_col: str = "name",
                                       make_col: str = "make",
                                       model_col: str = "model"):
+
+    _api_car_makes = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes'
+    _headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
+
+    res = requests.get(_api_car_makes, headers=_headers).text[2:-2]
+
+    makes_json = json.loads(res)['Makes']
+    car_makes = [x['make_display'] for x in makes_json]
+
     def _parse_makes(input_string):
         result_matches = [x for x in input_string.split() if x in car_makes]
         print(result_matches)
