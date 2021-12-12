69005b1d6a441c6231a7c4f3a55124f44a7ff952
lewandowskicommits <vesilth@gmail.com>
2021-12-12 17:39:13 +0100

    changed __main__ block to a pass

--- utilities.py
+++ utilities.py
@@ -22,26 +22,34 @@ def parse_makes_and_models_from_names(data: pd.DataFrame,
                                       make_col: str = "make",
                                       model_col: str = "model"):
 
-    _api_car_makes = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes'
-    _headers = {
+    api_car_makes = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes'
+    headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
 
-    res = requests.get(_api_car_makes, headers=_headers).text[2:-2]
+    res = requests.get(api_car_makes, headers=headers).text[2:-2]
 
-    makes_json = json.loads(res)['Makes']
-    car_makes = [x['make_display'] for x in makes_json]
+    car_makes_json = json.loads(res)['Makes']
+    car_makes = [x['make_display'] for x in car_makes_json]
+    car_makes_id = [x['make_id'] for x in car_makes_json]
+    car_makes_country = [x['make_country'] for x in car_makes_json]
+    car_makes_df = pd.DataFrame({'id': car_makes_id, 'name': car_makes, 'country': car_makes_country})
 
-    def _parse_makes(input_string):
-        result_matches = [x for x in input_string.split() if x in car_makes]
-        print(result_matches)
+    def _parse_makes(input_string, list_to_match):
+        result_matches = [x for x in input_string.split() if x in list_to_match]
 
         return result_matches
 
-    data[make_col] = data[name_col].apply(lambda x: _parse_makes(x))
-    return data
+    data[make_col] = data[name_col].apply(lambda x: _parse_makes(x, car_makes)).astype('str')
+    #result = pd.merge(data,
+    #                car_makes_df,
+    #                how='left',
+    #                left_on=[make_col],
+    #                right_on=['name']).fillna(0)
+
+    result = data
+
+    return result
 
 
 if __name__ == "__main__":
-    data = scrapers.scrape_otomoto()
-    # print(parse_engine_capacities_from_names(data))
-    print(parse_makes_and_models_from_names(data))
+    pass()
